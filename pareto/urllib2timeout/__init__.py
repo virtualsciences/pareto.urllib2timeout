4de7d0654f3464b18e999f62ea390cd885b84084
johnny <johnny@eris.(none)>
2013-04-25 14:09:16 +0200

    Has to work in Python 2.4, which doesn't support the timeout arg to urlopen, so using socket.setdefaulttimeout() now instead.

--- pareto/urllib2timeout/__init__.py
+++ pareto/urllib2timeout/__init__.py
@@ -1,9 +1,4 @@
-import urllib2
+import socket
 import settings
 
-org_urlopen = urllib2.urlopen
-def monkey(url, data=None, timeout=None):
-    if timeout is None:
-        timeout = settings.TIMEOUT
-    return org_urlopen(url, data, timeout)
-urllib2.urlopen = monkey
+socket.setdefaulttimeout(settings.TIMEOUT)
